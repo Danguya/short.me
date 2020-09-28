98e475f36d73b56272bfd56e9f7a2ed4e961e19a
danguya <daniel.yava16@gmail.com>
2020-09-28 22:14:42 +0100

    Editando os arquivos da página login

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login/login';
+import Signup from './pages/Signup/signup';
 
 export default function router() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path='/' exact component={Login} />
+                <Route path='/login' exact component={Login} />
+                <Route path='/signup'  component={Signup} />
             </Switch>
         </BrowserRouter>
 
